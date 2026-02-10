6265a1de9a3d08cfbc6875eb322d7131fdeaf1cb
Thilo Maier <git@maier.asia>
2026-02-10 17:33:54 +0100

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,4 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
-});
+export default defineConfig({ plugins: [sveltekit(), devtoolsJson()] });
